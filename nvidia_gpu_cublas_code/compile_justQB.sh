1038496c1d93a3d3f156779ee31d26a5dee429b8
Carolin Penke <c.penke@fz-juelich.de>
2026-08-19 14:13:31 +0200

    clean up

--- nvidia_gpu_cublas_code/compile_justQB.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /opt/intel/oneapi/setvars.sh
-module use /opt/nvidia/hpc_sdk/modulefiles/
-module load nvhpc
-export MKL_THREADING_LAYER=GNU
-
-nvcc driver_mkl_and_cublas_justQB.c rank_revealing_algorithms_mkl_and_cublas.c matrix_vector_functions_mkl_and_cublas.c -o driver_mkl_and_cublas_justQB -Xcompiler -fopenmp -lmkl_rt -lcublas -lcudart
